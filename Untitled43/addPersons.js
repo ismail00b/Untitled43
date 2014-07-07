9e122bd0028e5c80f8cdc4683877fe68cb7bf14a
Ismail Bougnouch <ismail00b@gmail.com>
2014-07-07 09:56:56 +0200

    Update #8

--- Untitled43/addPersons.js
+++ Untitled43/addPersons.js
@@ -1,6 +1,6 @@
-﻿for (var i = 0; i < 20; i++) {
-//	(new ds.Person({name : "name "+i, age :  Math.floor((Math.random() * 100) + 1)})).save();
-arr1.push({name : "name "+i, age :  Math.floor((Math.random() * 100) + 1)});
+﻿//for (var i = 0; i < 20; i++) {
+////	(new ds.Person({name : "name "+i, age :  Math.floor((Math.random() * 100) + 1)})).save();
+//arr1.push({name : "name "+i, age :  Math.floor((Math.random() * 100) + 1)});
 
-};
-source.arr1.sync();
+//};
+//source.arr1.sync();
